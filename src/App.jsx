529e11d236eb0620c4bbe6ee5fa06aec9dc69442
shibin <shibinashraf36@gmail.com>
2023-09-05 11:16:26 +0530

    files upload drag and drop added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,56 @@
 import React, { useState } from "react";
-import logo from './assets/logo.svg'
-import "./index.css"
-import { Uploader } from "./components/Uploader";
+import logo from "./assets/logo.svg";
+import "./index.css";
+import Uploader from "./components/Uploader";
+
 export default function App() {
   const [uploadMode, setUploadMode] = useState(false);
-  const UploadHandler= () =>{
-    setUploadMode(!uploadMode)
-    console.log(uploadMode)
-  }
+
+  const UploadHandler = () => {
+    setUploadMode(!uploadMode);
+    console.log(uploadMode);
+  };
+
   return (
-    
     <div className="z-[-1] min-w-screen min-h-screen bg no-scrollbar">
-      
-      <nav className="h-[7rem] w-screen text-white font-bold text-3xl p-12 days flex flex-row items-center"><img className="mr-4 h-12 w-12 "src={logo} alt="Logo" />PDF BIND
-     </nav>
-      <div className="h-[35rem]  items-center flex flex-col p-8">
-        <div className="text-3xl text-white font-bold mt-6 istok">Upload your PDF</div>
-        <div className="text-white text-xl font-thin my-6 istok">Merge pdf with ease.</div>
-                <div onClick={UploadHandler} className=" text-center shadow-xl text-lg rounded-xl py-4 px-6 w-[7rem] bg-[#845EC2] hover:scale-105 ease-in-out duration-300 text-white ">Upload</div>
-                
+      <nav className="h-[7rem] w-screen text-white font-bold text-3xl p-12 days flex flex-row items-center">
+        <img className="mr-4 h-12 w-12" src={logo} alt="Logo" />
+        PDF BIND
+      </nav>
+      {uploadMode && (
+        <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-75 z-50 flex justify-center items-center">
+          <div className="white-box p-6 bg-white shadow-md rounded-lg">
+            <div className="text-right mb-2">
+              <button
+                onClick={UploadHandler}
+                className="text-gray-600 hover:text-gray-800"
+              >
+                Close
+              </button>
+            </div>
+            <Uploader />
+            <div className="mt-4 text-center">
+              <button className="px-4 py-2 bg-[#845EC2] text-white rounded-md hover:bg-opacity-80">
+                Merge PDF
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="h-[35rem] items-center flex flex-col p-8">
+        <div className="text-3xl text-white font-bold mt-6 istok">
+          Upload your PDF
+        </div>
+        <div className="text-white text-xl font-thin my-6 istok">
+          Merge pdf with ease.
+        </div>
+        <div
+          onClick={UploadHandler}
+          className="text-center shadow-xl text-lg rounded-xl py-4 px-6 w-[7rem] bg-[#845EC2] hover:scale-105 ease-in-out duration-300 text-white"
+        >
+          Upload
+        </div>
       </div>
-      
     </div>
   );
-  
- 
 }
-
-
--- src/components/Uploader.jsx
+++ src/components/Uploader.jsx
@@ -1,7 +1,85 @@
-import React from 'react'
+import React, { useState, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-export const Uploader = () => {
+export default function Uploader() {
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    // Create an array of file objects with additional data for order and key
+    const newFiles = acceptedFiles.map((file, index) => ({
+      file,
+      order: index,
+      key: Date.now() + index,
+    }));
+
+    setUploadedFiles((prevFiles) => [...prevFiles, ...newFiles]);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: ".pdf",
+    onDrop,
+  });
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const items = Array.from(uploadedFiles);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    // Update the order property for each item
+    const updatedItems = items.map((item, index) => ({
+      ...item,
+      order: index,
+    }));
+
+    setUploadedFiles(updatedItems);
+  };
+
   return (
-    <div className='z-1 flex justify-center items-center h-[40rem] bg-white'>Uploader</div>
-  )
+    <div className="white-box p-6 bg-white shadow-md rounded-lg">
+      <div {...getRootProps()} className="border-2 border-dashed border-gray-300 p-4 text-center cursor-pointer">
+        <input {...getInputProps()} />
+        <p className="text-gray-400 text-lg">Drag & drop PDF files here, or click to select files</p>
+      </div>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId={`file-list-${Math.random()}`}>
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {uploadedFiles.map((item, index) => (
+                <Draggable key={item.key} draggableId={item.key.toString()} index={index}>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className="bg-gray-200 p-4 mt-4 rounded-md"
+                    >
+                      <div className="flex justify-between items-center">
+                        <span>{item.file.name}</span>
+                        <button
+                          className="text-red-600 hover:text-red-800"
+                          onClick={() => {
+                            // Remove the file from the list
+                            const updatedFiles = uploadedFiles.filter(
+                              (file) => file.key !== item.key
+                            );
+                            setUploadedFiles(updatedFiles);
+                          }}
+                        >
+                          Remove
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
 }
